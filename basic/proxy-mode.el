5acd9329a3925cd03427247e8410372ee05b4c0b
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-05 12:20:19 -0800

    proxy-mode

---
+++ basic/proxy-mode.el
@@ -0,0 +1,44 @@
+;;; proxy-mode.el --- 
+
+;; Copyright (C) 2016  Ernesto Alfonso <erjoalgo@gmail.com>
+
+;; Author: Ernesto Alfonso <erjoalgo@gmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defun proxy-on ()
+  (interactive)
+  (proxy-internal t))
+
+(defun proxy-off ()
+  (interactive)
+  (proxy-internal nil))
+
+(defun proxy-internal (on)
+  (let ((value (if on proxy-mode-proxy ""))
+	(envs '("http_proxy" "http_proxy" "HTTP_PROXY" "HTTPS_PROXY")))
+    (message "value is: %s" value)
+    (dolist (env envs)
+      (setenv env value))))
+
+(defcustom proxy-mode-proxy "" "proxy for proxy mode")
+    
+
+(provide 'proxy-mode)
+;;; proxy-mode.el ends here
