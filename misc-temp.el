8e3eef3e8627660db8bcc2b95ee5348a0cbdf7a7
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-10 09:19:03 -0800

    go settings

--- misc-temp.el
+++ misc-temp.el
@@ -1,15 +1,9 @@
 ;;minibuffer-local-map
+(load-file (f-join "~/python-buttons.el"))
 (elisp_install_buttons read-expression-map)
 (add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
 (add-hook 'ielm-mode-hook 'elisp_install_buttons)
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
 
-
-
 (add-to-list 'load-path (f-join (getenv "GOPATH")
 				"src/github.com/golang/lint/misc/emacs"))
-(require 'golint)
-
-(setq gofmt-command
-      (f-join (getenv "GOPATH") "bin/goimports"))
-(add-hook 'before-save-hook #'gofmt-before-save)
