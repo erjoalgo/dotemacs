ffa22005d986a7379f15834975e5aab626915134
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-14 00:39:28 -0700

    read-expression-map install buttons

--- misc-temp.el
+++ misc-temp.el
@@ -1,6 +1,6 @@
 ;;minibuffer-local-map
 (load-file (f-join emacs-top "in-progress/python_buttons.el"))
-(elisp_install_buttons read-expression-map)
+
 (add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
 (add-hook 'ielm-mode-hook 'elisp_install_buttons)
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
@@ -8,3 +8,4 @@
 (when (getenv "GOPATH")
   (add-to-list 'load-path (f-join (getenv "GOPATH")
 				"src/github.com/golang/lint/misc/emacs")))
+(elisp_install_buttons read-expression-map)
