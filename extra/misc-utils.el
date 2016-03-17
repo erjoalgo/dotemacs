98511e6fb3984360c17d9878eb88a2b80cb297b1
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-16 19:35:05 -0700

    remove nil idiom

--- extra/misc-utils.el
+++ extra/misc-utils.el
@@ -1,8 +1,8 @@
 (defun whereis (program)
-  (remove-if-not
-   'identity
-   (loop for dir in (split-string (getenv "PATH") ":" t)
-	 if (file-exists-p dir)
-	 collect (loop for fn in (directory-files dir) thereis
-		       (and (string= program fn)
-			    (f-join dir fn))))))
+  ;;(interactive "senter program: ")
+  (remove nil
+	  (loop for dir in (split-string (getenv "PATH") ":" t)
+		if (file-exists-p dir)
+		collect (loop for fn in (directory-files dir) thereis
+			      (and (string= program fn)
+				   (f-join dir fn))))))
