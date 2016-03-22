310d810a42f8e07c34ebfc707278d5723286d347
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-22 00:42:06 -0700

    smaller misc utils

--- extra/misc-utils.el
+++ extra/misc-utils.el
@@ -1,53 +1,26 @@
 (defun whereis (program)
   (interactive "senter program: ")
-  (let (dirs)
-    (setf dirs
-	  (remove
-	   nil
-	   (loop for dir in (split-string (getenv "PATH") ":" t)
-		 if (file-exists-p dir)
-		 collect (loop for fn in (directory-files dir)
-			       thereis
-			       (and (string= program fn)
-				    (f-join dir fn))))))
-    (if (called-interactively-p 'interactively)
-	(message "%s" (remove nil dirs))
-      (remove nil dirs))))
+  (let ((dirs
+	 (loop for dir in (split-string (getenv "PATH") ":" t)
+	       if (and (file-exists-p dir)
+		       (member program (directory-files dir)))
+	       collect (f-join dir program))))
+
+    (when (called-interactively-p 'interactively)
+      (message "%s" dirs))
+  dirs))
 
 (defun recover-this-file-and-diff ()
   (interactive)
   (recover-this-file);;TODO ignore prompt
   (diff-buffer-with-file))
 
-
 (defun shell-command-of-region (a b)
+  "compare to shell-command-on-region"
   (interactive "r")
   ;(shell-command-to-string (buffer-substring a b))
   (async-shell-command (buffer-substring a b)))
 
-;;stands for stack exchange...
-(defun sexchange-insert-cmd-and-output (cmd)
-  (interactive (list (read-shell-command
-		      "senter shell command: ")))
-  (insert (format "\n\n%s\n%s\n\n"
-		  (sexchange-code-block
-		   (concat "$" cmd))
-		  (sexchange-code-block (shell-command-to-string cmd)))))
-
-(defun sexchange-code-block (code)
-  (replace-regexp-in-string
-   "^" "    " code))
-
-
-(defun sexchange-code-block-region (a b)
-  (interactive "r")
-  (save-excursion
-    (goto-char a)
-    (while (< (point) b)
-      (beginning-of-line)
-      (insert "    ")
-      (next-logical-line))))
-  
 (defun lnabs (source &optional prompt)
   (interactive "fEnter soft link source: ")
   (let* ((source (expand-file-name source))
@@ -56,11 +29,3 @@
 	 (command (format "ln -sf %s %s" source (expand-file-name destination))))
     (when (or (not prompt) (y-or-n-p command))
       (shell-command command))))
-
-(defun message-current-buffer-process ()
-  (interactive)
-  (let ((proc (get-buffer-process (current-buffer))))
-    (if proc
-	(message "%s" (process-command proc))
-      (message "buffer has no process"))))
-
