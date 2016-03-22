c6e3a66a1e0d1a0e021df99a24a213f536534c38
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-21 21:22:46 -0700

    basic org-mode bindings

--- settings/org-mode-settings.el
+++ settings/org-mode-settings.el
@@ -1,6 +1,20 @@
+(defun my-org-mode-settings ()
+  (define-key org-mode-map (kbd "M-]") 'org-metaright);;promote
+  (define-key org-mode-map (kbd "M-[") 'org-metaleft);;demote
+  (define-key org-mode-map (kbd "RET") 'org-meta-return);;insert new
+  
+  (define-key org-mode-map (kbd "s-t")
+    (lambda () (interactive) (org-todo 1)));;TODO
+  (define-key org-mode-map (kbd "s-T")
+    (lambda () (interactive) (org-todo 2)));;DONE
+  (define-key org-mode-map (kbd "s-d")
+    'org-deadline)
+  
+  (setq org-blank-before-new-entry
+	;don't add extra newlines
+	'((heading . nil)
+	  (plain-list-item . nil))
+	;;don't cut the current line
+	org-M-RET-may-split-line '((default . nil ))))
 
-(when nil
-  ;;should be unnecessary in newer emacs
-  (add-to-list 'load-path "~/programs/org-8.3.4/")
-  (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode)))
-
+(eval-after-load 'org 'my-org-mode-settings)
