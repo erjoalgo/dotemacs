9b04fb537a129225d4252439511e951aa917290b
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-22 00:42:26 -0700

    remove go settings

--- misc-temp.el
+++ misc-temp.el
@@ -1,14 +1,12 @@
 ;;minibuffer-local-map
-(load-file (f-join emacs-top "in-progress/python_buttons.el"))
+(load-file
+ (f-join emacs-top "in-progress/python_buttons.el"))
 
-(add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
+
 (add-hook 'ielm-mode-hook 'elisp_install_buttons)
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
 
-(when (getenv "GOPATH")
-  (add-to-list 'load-path (f-join (getenv "GOPATH")
-				"src/github.com/golang/lint/misc/emacs")))
-(elisp_install_buttons read-expression-map)
+
 
 (defun upcase-last ()
   (interactive)
