5cbc2cf2ad25fe2b0236695d050cde9b750889b2
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-23 16:13:42 -0700

    wip untar-last-downloaded-program

--- extra/misc-utils.el
+++ extra/misc-utils.el
@@ -29,3 +29,25 @@
 	 (command (format "ln -sf %s %s" source (expand-file-name destination))))
     (when (or (not prompt) (y-or-n-p command))
       (shell-command command))))
+
+(cl-defun sort-key (list key &key descending (pred '<))
+  (let* ((sorted-tuples (sort (mapcar (lambda (el)
+				(cons el (funcall key el)))
+			      list)
+		      (lambda (a b)
+			(funcall pred (cdr a) (cdr b)))))
+	(sorted (mapcar 'car sorted-tuples)))
+    (if descending (reverse sorted)
+      sorted)))
+
+  
+			
+  
+(defun directory-files-sort-by-ctime-descending (dir)
+  (let ((files (directory-files dir)))
+    (reverse (sort-key files (lambda (fn)
+		  (let ((attrs (file-attributes (f-join dir fn))))
+		    (nth 6 attrs)))))))
+
+  
+		  
