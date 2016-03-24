c28fdc881d6310ee41751d10d888f21cebb6b7a8
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-23 21:54:10 -0700

    move buttons hooks to misc

--- misc-temp.el
+++ misc-temp.el
@@ -5,6 +5,8 @@
 
 (add-hook 'ielm-mode-hook 'elisp_install_buttons)
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
+(add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
+(elisp_install_buttons read-expression-map)
 
 
 
