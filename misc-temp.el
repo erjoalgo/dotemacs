b6cbafe4d2f8af13728fe530bd32afda0c72fc99
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-24 12:59:50 -0700

    some common modes, pending full refactoring of python buttons

--- misc-temp.el
+++ misc-temp.el
@@ -6,7 +6,11 @@
 (add-hook 'ielm-mode-hook 'elisp_install_buttons)
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
 (add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
+(add-hook 'lisp-mode-hook 'lisp_install_buttons)
+(add-hook 'lisp-mode-hook 'slime-mode)
 (elisp_install_buttons read-expression-map)
+
+(add-hook 'shell-script-mode 'bash_install_buttons)
 
 
 
