d8bfb21f65f5167111e7d6f97e606cbee10ef9bb
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-23 21:49:07 -0700

    untested: golint fails to install

--- settings/go-settings.el
+++ settings/go-settings.el
@@ -1,5 +1,19 @@
-(require 'golint)
+
 (require 'f)
 
-(setq gofmt-command (f-join (getenv "GOPATH") "bin/goimports"))
-(add-hook 'before-save-hook #'gofmt-before-save)
+(let* ((GOPATH (getenv "GOPATH"))
+      (go-lint-path (f-join GOPATH "src/github.com/golang/lint/misc/emacs")))
+  
+  (if (not (and GOPATH (file-exists-p go-lint-path)))
+      (message "WARNING: go or go lint not installed (go get github.com/golang/lint)")
+    (progn 
+      (add-to-list 'load-path go-lint-path)
+      (setq gofmt-command (f-join GOPATH "bin/goimports"))
+      (eval-after-load 'go-mode
+	(lambda () 
+	  (add-hook 'before-save-hook #'gofmt-before-save)
+	  (require 'golint))))))
+
+(add-to-list 'auto-mode-alist '("[.]go$" . go-mode))
+
+
