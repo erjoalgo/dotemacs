3a40295fa9360cf9bd938eaafcf998969cbb7f6a
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-24 13:00:23 -0700

    slime settings, pending send initial sexps

--- settings/slime-settings.el
+++ settings/slime-settings.el
@@ -6,16 +6,31 @@
 (setq inferior-lisp-program "sbcl")
 (setq slime-contribs '(slime-fancy))
 
-(defun sbcl-slime ()
+(defun find-buffer-by-starts-with (prefix)
+  (loop for buff in (buffer-list)
+	thereis (and (s-starts-with-p
+		      prefix
+		      (buffer-name buff))
+		     buff)))
+
+(defun slime-sbcl ()
   (interactive)
   (let* ((slime-sbcl-buffer-name "*slime-repl sbcl")
-	 (slime-sbcl-buffer (loop for buff in (buffer-list)
-				  thereis (and (s-starts-with-p
-						slime-sbcl-buffer-name
-						(buffer-name buff))
-					       buff))))
+	 (slime-sbcl-buffer (find-buffer-by-starts-with slime-sbcl-buffer-name)))
 
     (if slime-sbcl-buffer
 	(switch-to-buffer slime-sbcl-buffer)
       ;;(add-hook 'slime-connected-hook 'load-compiler-hook)
       (slime))))
+
+(defvar *stumpwm-swank-port* 4005)
+(defun slime-stumpwm ()
+  (interactive)
+  (let ((slime-stumpwm-buffer
+	 (find-buffer-by-starts-with "*slime-repl sbcl")))
+    (if slime-stumpwm-buffer
+	(switch-to-buffer slime-stumpwm-buffer)
+      (slime-connect "localhost" *stumpwm-swank-port*))))
+
+  
+(require 'slime)
