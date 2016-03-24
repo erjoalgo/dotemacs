b70f86622a478b997290fa018d5645bde063a006
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-24 13:00:46 -0700

    metaup, metadown, insert-link

--- settings/org-mode-settings.el
+++ settings/org-mode-settings.el
@@ -1,14 +1,22 @@
 (with-eval-after-load 'org
+  (define-key org-mode-map (kbd "M-P") 'org-metaup);;move up
+  (define-key org-mode-map (kbd "M-N") 'org-metadown);;move down
+  
   (define-key org-mode-map (kbd "M-]") 'org-metaright);;promote
   (define-key org-mode-map (kbd "M-[") 'org-metaleft);;demote
+  
   (define-key org-mode-map (kbd "RET") 'org-meta-return);;insert new
+					;use C-j to add text
   
   (define-key org-mode-map (kbd "s-t")
-    (lambda () (interactive) (org-todo 1)));;TODO
+    (lambda () (interactive) (org-todo 1)));;tag TODO
   (define-key org-mode-map (kbd "s-T")
-    (lambda () (interactive) (org-todo 2)));;DONE
+    (lambda () (interactive) (org-todo 2)));;tag DONE
   (define-key org-mode-map (kbd "s-d")
     'org-deadline)
+
+  (define-key org-mode-map (kbd "s-l")
+    'org-insert-link)
   
   (setq org-blank-before-new-entry
 					;don't add extra newlines
@@ -17,8 +25,7 @@
 	;;don't cut the current line
 	org-M-RET-may-split-line '((default . nil )))
 
-  (setq org-startup-folded nil)
-  )
+  (setq org-startup-folded nil))
 
 (defvar notes-file
   (f-join emacs-top "org" "notes.org"))
