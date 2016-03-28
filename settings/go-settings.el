7c9ff02b575f13203e20032cb7253593df5611b2
Ernesto Alfonso <erjoalgo@gmail.com>
2016-03-28 15:11:17 -0700

    fix go-settings

--- settings/go-settings.el
+++ settings/go-settings.el
@@ -1,17 +1,17 @@
 
 (require 'f)
 
-(let* ((GOPATH (getenv "GOPATH"))
-      (go-lint-path (f-join GOPATH "src/github.com/golang/lint/misc/emacs")))
-  
-  (if (not (and GOPATH (file-exists-p go-lint-path)))
-      (message "WARNING: go or go lint not installed (go get github.com/golang/lint)")
-    (progn 
-      (add-to-list 'load-path go-lint-path)
-      (setq gofmt-command (f-join GOPATH "bin/goimports"))
-      (eval-after-load 'go-mode
-	(lambda () 
-	  (add-hook 'before-save-hook #'gofmt-before-save)
-	  (require 'golint))))))
+(let ((GOPATH (getenv "GOPATH")))
+  (when GOPATH
+    (let ((go-lint-path (f-join GOPATH "src/github.com/golang/lint/misc/emacs"))
+	  (goimports-path (f-join GOPATH "bin/goimports")))
+      
+      (if (not (file-exists-p go-lint-path))
+	  (message "WARNING: go or go lint not installed (go get github.com/golang/lint)")
+	(add-to-list 'load-path go-lint-path))
+      
+      (if (not (file-exists-p goimports-path))
+	  (message "WARNING: go or go lint not installed (go get github.com/golang/lint)")
+	(setf gofmt-command goimports-path)))))
 
 (add-to-list 'auto-mode-alist '("[.]go$" . go-mode))
