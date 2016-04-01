27e6fa8ac1c55b473016c21884ad2f7aab0691ec
Ernesto Alfonso <erjoalgo@gmail.com>
2016-04-01 14:32:09 -0700

    more hooks

--- misc-temp.el
+++ misc-temp.el
@@ -7,11 +7,13 @@
 (add-hook 'slime-repl-mode-hook 'elisp_install_buttons)
 (add-hook 'emacs-lisp-mode-hook 'elisp_install_buttons)
 (add-hook 'lisp-mode-hook 'cl_install_buttons)
+(add-hook 'go-mode-hook 'go_install_buttons)
 
 (add-hook 'lisp-mode-hook 'slime-mode)
 (elisp_install_buttons read-expression-map)
 
-(add-hook 'shell-script-mode 'bash_install_buttons)
+;(add-hook 'shell-script-mode-hook 'bash_install_buttons)
+(add-hook 'sh-mode-hook 'bash_install_buttons)
 
 
 
@@ -39,6 +41,7 @@
 (defun firefox-new-tab-sh (url &optional unknown-arg)
   (let ((new-tab "new-tab"))
     (start-process new-tab new-tab
-		 "firefox-new-tab.sh"
-		 url)))
+		   "firefox-new-tab.sh"
+		   url)))
 (setq browse-url-browser-function 'firefox-new-tab-sh)
+
