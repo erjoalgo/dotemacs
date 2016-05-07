2c3e24ab3ece365fa4a9bfba401006bb6d8a4a74
Ernesto Alfonso <erjoalgo@gmail.com>
2016-05-07 16:54:24 -0700

    wrap in with-eval-after-load

--- settings/ac-mode-settings.el
+++ settings/ac-mode-settings.el
@@ -1,27 +1,24 @@
-
-
-(setf ac-number-candidates-p t)
-(setf ac-complete-select-nth-kdb-fun
+(with-eval-after-load "auto-complete"
+  (setf ac-number-candidates-p t)
+  (setf ac-complete-select-nth-kdb-fun
       (lambda (i) (kbd (format "s-%d" i))))
-
-
+  
 ;;(setf ac-delay .005)
-(setf ac-delay 0)
-(setf ac-auto-show-menu 0)
+  (setf ac-delay 0)
+  (setf ac-auto-show-menu 0)
 
 ;;start when first character typed, don't wait until 2nd
-(setf ac-auto-start 1)
+  (setf ac-auto-start 1)
 
-
-(ac-config-default)
+  (ac-config-default)
 
 ;;command-mode and goto-last-change
-(define-key ac-completing-map [f1] nil )
-(define-key ac-completing-map [M-f1] nil )
+  (define-key ac-completing-map [f1] nil )
+  (define-key ac-completing-map [M-f1] nil ))
 
 (defalias 'acmode 'auto-complete-mode)
 
+;;ac mode slows down netbook too much
 (unless (member system-name '("debian-mini"))
-  ;;ac mode slows down netbook too much
   (require 'auto-complete)
   (require 'auto-complete-config))
