bfff62769025f897acce7af6fbed58a21d69167b
Ernesto Alfonso <erjoalgo@gmail.com>
2016-05-10 09:24:41 -0700

    no need for with-eval-fter-load

--- settings/org-mode-settings.el
+++ settings/org-mode-settings.el
@@ -1,48 +1,53 @@
-(with-eval-after-load 'org
-  (define-key org-mode-map (kbd "M-P") 'org-metaup);;move up
-  (define-key org-mode-map (kbd "M-N") 'org-metadown);;move down
+(require  'org)
+
+(defmacro define-keys (kmap &rest bindings)
+  `(progn ,@(loop for (key cmd) in bindings collect
+		  `(define-key ,kmap
+		     ,(if (stringp key)
+			  `(kbd ,key) ,key)
+		     ,cmd))))
+
+(define-keys org-mode-map
+  ("M-P" 'org-metaup);;move up
+  ("M-N" 'org-metadown);;move down
   
-  (define-key org-mode-map (kbd "M-]") 'org-metaright);;promote
-  (define-key org-mode-map (kbd "M-[") 'org-metaleft);;demote
+  ("M-]" 'org-metaright);;promote
+  ("M-[" 'org-metaleft);;demote
 
 
-  (define-key org-mode-map (kbd "C-M-]") 'org-demote-subtree);;promote
-  (define-key org-mode-map (kbd "C-M-[") 'org-promote-subtree);;demote
+  ("C-M-]" 'org-demote-subtree);;promote
+  ("C-M-[" 'org-promote-subtree);;demote
   
-  (define-key org-mode-map (kbd "RET") 'org-meta-return);;insert new
+  ("RET" 'org-meta-return);;insert new
 					;use C-j to add text
   
-  (define-key org-mode-map (kbd "s-1")
-    (lambda () (interactive) (org-todo 1)));;tag TODO
-  (define-key org-mode-map (kbd "s-2")
-    (lambda () (interactive) (org-todo 2)));;tag DONE
-  (define-key org-mode-map (kbd "s-d")
-    'org-deadline)
+  ("s-1" (lambda () (interactive) (org-todo 1)));;tag TODO
+  ("s-2" (lambda () (interactive) (org-todo 2)));;tag DONE
+  ("s-d" 'org-deadline)
 
-  (define-key org-mode-map (kbd "s-l")
-    'org-insert-link)
-  
-  (define-key org-mode-map (kbd "s-0") 'org-ctrl-c-ctrl-c)
+  ("s-l" 'org-insert-link)
+  ("s-0" 'org-ctrl-c-ctrl-c))
 
-  (setq org-blank-before-new-entry
-					;don't add extra newlines
-	'((heading . nil)
-	  (plain-list-item . nil))
-	;;don't cut the current line
-	org-M-RET-may-split-line '((default . nil )))
-  
-  (setq org-startup-folded nil))
+(setq org-blank-before-new-entry
+      ;;don't add extra newlines
+      '((heading . nil)
+	(plain-list-item . nil))
+      ;;don't cut the current line
+      org-M-RET-may-split-line '((default . nil )))
+
+(setq org-startup-folded nil)
 
 (defvar notes-file
   (f-join emacs-top "org"))
 
-
-
 (defvar *org-todo-first-todo-line-number* 3)
+
 (defun org-todo-promote-top ()
   (interactive)
   (save-excursion
-    (move-line-up (- (line-number-at-pos) *org-todo-first-todo-line-number*))))
+    (move-line-up (- (line-number-at-pos)
+		     *org-todo-first-todo-line-number*))))
+
 (with-eval-after-load 'org-agenda
   (define-key org-agenda-mode-map (kbd "s-q") 'org-todo-promote-top))
 
@@ -72,27 +77,31 @@
       (replace-match
        (concat (make-string (1- (length (match-string 1))) 32) "-")))))
 
-(with-eval-after-load "org"
-  (define-key org-mode-map (kbd "M-c") 'org-export-mine)
-  (setq org-html-validation-link nil)
-  
-  (add-hook 'after-save-hook (lambda ()
-			       (when (and (eq major-mode 'org-mode)
-					  (equal (f-filename (buffer-file-name))
-						 "README.org"))
-				 (org-md-export-to-markdown)))))
+(define-key org-mode-map (kbd "M-c") 'org-export-mine)
+(setq org-html-validation-link nil)
 
-(with-eval-after-load "org"
-  (require 'org-crypt)
-  (org-crypt-use-before-save-magic)
-  (setq org-tags-exclude-from-inheritance
-	'("crypt"))
-  (require 'epa)
-  (setq org-crypt-key "AFF54E1E");; erjoalgo@gmail.com
+(add-hook 'after-save-hook (lambda ()
+			     (when (and (eq major-mode 'org-mode)
+					(equal (f-filename (buffer-file-name))
+					       "README.org"))
+			       (org-md-export-to-markdown))))
+
+(require 'org-crypt)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance
+      '("crypt"))
+(require 'epa)
+(setq org-crypt-key "AFF54E1E");; erjoalgo@gmail.com
+
+(define-keys org-agenda-mode-map
+  ("s-1" (lambda () (interactive) (org-agenda-todo 1)));;tag TODO
+  ("s-2" (lambda () (interactive) (org-agenda-todo 2)));;tag DONE
   )
 
-(with-eval-after-load "org"
-  (when (file-exists-p notes-file)
+(when (file-exists-p notes-file)
   (push notes-file org-agenda-files)
-  (org-todo-list org-match)
-  (org-agenda-list nil)))
+  (org-agenda-list nil)
+  (setq initial-buffer-choice
+	(lambda ()
+	  (org-todo-list org-match)))
+  (org-todo-list org-match))
