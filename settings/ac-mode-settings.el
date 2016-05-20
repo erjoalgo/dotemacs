08a9e17c49676c2926c20b54fdd9e3616ca64d4f
Ernesto Alfonso <erjoalgo@gmail.com>
2016-05-20 15:02:58 -0700

    reorder to fix error

--- settings/ac-mode-settings.el
+++ settings/ac-mode-settings.el
@@ -1,24 +1,23 @@
 (with-eval-after-load "auto-complete"
   (setf ac-number-candidates-p t)
   (setf ac-complete-select-nth-kdb-fun
-      (lambda (i) (kbd (format "s-%d" i))))
+	(lambda (i) (kbd (format "s-%d" i))))
   
-;;(setf ac-delay .005)
+  ;;(setf ac-delay .005)
   (setf ac-delay 0)
   (setf ac-auto-show-menu 0)
 
-;;start when first character typed, don't wait until 2nd
+  ;;start when first character typed, don't wait until 2nd
   (setf ac-auto-start 1)
 
-  (ac-config-default)
-
-;;command-mode and goto-last-change
+  ;;command-mode and goto-last-change
   (define-key ac-completing-map [f1] nil )
-  (define-key ac-completing-map [M-f1] nil ))
+  (define-key ac-completing-map [M-f1] nil))
 
 (defalias 'acmode 'auto-complete-mode)
 
 ;;ac mode slows down netbook too much
 (unless (member system-name '("debian-mini"))
   (require 'auto-complete)
-  (require 'auto-complete-config))
+  (require 'auto-complete-config)
+  (ac-config-default))
