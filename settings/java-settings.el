52203d0b6473eb0462d554fd7898e43e669b1081
Ernesto Alfonso <erjoalgo@gmail.com>
2016-06-06 13:49:50 -0700

    java actually uses 2-space indentation?

--- settings/java-settings.el
+++ settings/java-settings.el
@@ -1,2 +1,6 @@
 (add-hook 'java-mode-hook (lambda ()
-			    (setf tab-width 4)))
+			    ;(setf tab-width 8)
+			    (setf tab-width 2)
+                            (setf indent-tabs-mode nil)))
+
+
