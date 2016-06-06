948b8e4c9b30a996a475ea71d93fec32a0582e90
Ernesto Alfonso <erjoalgo@gmail.com>
2016-06-06 13:50:46 -0700

    with-eval-after-load slime to fix crash

--- settings/company-settings.el
+++ settings/company-settings.el
@@ -1,9 +1,10 @@
 (add-hook-to-modes 'company-mode '(emacs-lisp-mode slime-mode))
 (define-key emacs-lisp-mode-map (kbd "TAB") 'company-complete)
-(define-key slime-mode-map (kbd "TAB") 'company-complete)
+(with-eval-after-load "slime"
+(define-key slime-mode-map (kbd "TAB") 'company-complete))
 
 (define-key company-active-map (kbd "<f1>") nil)
 (define-key company-active-map (kbd "C-h") 'company-show-doc-buffer)
 (dotimes (i 10)
   (define-key company-active-map (read-kbd-macro (format "<s-%d>" i))
-    'company-complete-number))
+    `(lambda () (interactive) (company-complete-number ,i))))
