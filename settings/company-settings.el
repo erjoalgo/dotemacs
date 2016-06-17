1aa19a625ab0afa24c88a638282d2cb08307c757
Ernesto Alfonso <erjoalgo@gmail.com>
2016-06-17 09:08:53 -0700

    use s-fn for now in company-active-map

--- settings/company-settings.el
+++ settings/company-settings.el
@@ -1,19 +1,31 @@
 (add-hook-to-modes 'company-mode '(emacs-lisp-mode slime-mode go-mode))
 (define-key emacs-lisp-mode-map (kbd "TAB") 'company-complete)
+
 (with-eval-after-load "slime"
-(define-key slime-mode-map (kbd "TAB") 'company-complete))
+  (define-key slime-mode-map (kbd "TAB") 'company-complete))
 
 (define-key company-active-map (kbd "<f1>") nil)
 (with-eval-after-load "go-mode"
-(define-key go-mode-map (kbd "TAB") 'company-complete))
+  (define-key go-mode-map (kbd "TAB") 'company-complete))
 
 (define-key company-active-map (kbd "C-h") 'company-show-doc-buffer)
+
+(dotimes (i 10)
+  (define-key company-active-map (kbd (format "<s-%d>" i))
+    `(lambda () (interactive) (message "company called %s" ,i)
+       (company-complete-number ,i))))
+
 (dotimes (i 10)
   (define-key company-active-map (read-kbd-macro (format "<s-%d>" i))
-    `(lambda () (interactive) (company-complete-number ,i))))
+    `(lambda () (interactive) (company-complete-number ,i)))
+  (define-key company-active-map (kbd (format "<s-f%d>" i))
+    `(lambda () (interactive)
+       (message "company called %s" ,i)
+       (company-complete-number ,i))))
 
 
-;(global-set-key (kbd "<s-tab>") 'indent-according-to-mode)
+					;(global-set-key (kbd "<s-tab>") 'indent-according-to-mode)
+
 (global-set-key (kbd "<s-tab>") (lambda () (interactive)
 				  (if (region-active-p)
 				      (indent-region (region-beginning) (region-end))
