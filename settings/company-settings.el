0f2cf87ed9655aa0e388a20f9fc60c8cf09fec92
Ernesto Alfonso <erjoalgo@gmail.com>
2016-07-09 15:19:21 -0700

    with-eval-after-load wrap

--- settings/company-settings.el
+++ settings/company-settings.el
@@ -4,24 +4,28 @@
 (with-eval-after-load "slime"
   (define-key slime-mode-map (kbd "TAB") 'company-complete))
 
-(define-key company-active-map (kbd "<f1>") nil)
+
 (with-eval-after-load "go-mode"
   (define-key go-mode-map (kbd "TAB") 'company-complete))
 
-(define-key company-active-map (kbd "C-h") 'company-show-doc-buffer)
 
-(dotimes (i 10)
-  (define-key company-active-map (kbd (format "<s-%d>" i))
-    `(lambda () (interactive) (message "company called %s" ,i)
-       (company-complete-number ,i))))
+(with-eval-after-load "company"
+  (define-key company-active-map (kbd "<f1>") nil)
 
-(dotimes (i 10)
-  (define-key company-active-map (read-kbd-macro (format "<s-%d>" i))
-    `(lambda () (interactive) (company-complete-number ,i)))
-  (define-key company-active-map (kbd (format "<s-f%d>" i))
-    `(lambda () (interactive)
-       (message "company called %s" ,i)
-       (company-complete-number ,i))))
+  (define-key company-active-map (kbd "C-h") 'company-show-doc-buffer)
+
+  (dotimes (i 10)
+    (define-key company-active-map (kbd (format "<s-%d>" i))
+      `(lambda () (interactive) (message "company called %s" ,i)
+	 (company-complete-number ,i))))
+
+  (dotimes (i 10)
+    (define-key company-active-map (read-kbd-macro (format "<s-%d>" i))
+      `(lambda () (interactive) (company-complete-number ,i)))
+    (define-key company-active-map (kbd (format "<s-f%d>" i))
+      `(lambda () (interactive)
+	 (message "company called %s" ,i)
+	 (company-complete-number ,i)))))
 
 
 					;(global-set-key (kbd "<s-tab>") 'indent-according-to-mode)
