0e7d695061f30bb6045b93e12237977349ffe570
Ernesto Alfonso <erjoalgo@gmail.com>
2016-07-09 20:00:38 -0700

    try to automate psql connection

---
+++ lisp/settings/sql-settings.el
@@ -0,0 +1,10 @@
+(setf 
+ sql-postgres-login-params '((user :default "postgres")
+			     (password :default "postgres")
+			     (server :default "localhost")
+			     (database :default "" )))
+
+(setf sql-server "localhost"
+      sql-database ""
+      
+      )
