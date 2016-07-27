5558c02386dc28b7350482b39faca1943e804e6e
Ernesto Alfonso <erjoalgo@gmaill.com>
2016-07-26 19:57:21 -0700

    (warn ...) instead of (message "WARN ...")

--- lisp/settings/go-settings.el
+++ lisp/settings/go-settings.el
@@ -3,15 +3,17 @@
 
 (let ((GOPATH (getenv "GOPATH")))
   (when GOPATH
-    (let ((go-lint-path (f-join GOPATH "src/github.com/golang/lint/misc/emacs"))
+    (let ((go-lint-path
+	   (f-join GOPATH "src/github.com/golang/lint/misc/emacs"))
 	  (goimports-path (f-join GOPATH "bin/goimports")))
-      
+
       (if (not (file-exists-p go-lint-path))
-	  (message "WARNING: go or go lint not installed (go get github.com/golang/lint)")
+	  (warn (concat "WARNING: go or go lint not installed"
+			   "(go get github.com/golang/lint)"))
 	(add-to-list 'load-path go-lint-path))
-      
+
       (if (not (file-exists-p goimports-path))
-	  (message "WARNING: goimports not found (go get github.com/golang/lint)")
+	  (warn "WARNING: goimports not found (go get github.com/golang/lint)")
 	(setf gofmt-command goimports-path)
 	(add-hook 'before-save-hook #'gofmt-before-save)))))
 
