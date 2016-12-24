76521eef7756efaa9b8014595b61c217ad273fdc
Ernesto Alfonso <ernesto.alfonsogonzalez@ge.com>
2016-12-23 20:15:26 -0800

    gen-dot-gnus.el: use kw args to avoid imap/stmp confusion

--- lisp/settings/gen-dot-gnus.el
+++ lisp/settings/gen-dot-gnus.el
@@ -10,7 +10,7 @@
 	   (imap-server ,imap-server)
 	   (imap-port ,imap-port))
        (setf gnus-select-method
-	     `(nnimap "gmail"
+	     `(nnimap ,email
 		      (nnimap-address ,imap-server)
 		      (nnimap-server-port ,imap-port)
 		      (nnimap-stream ssl)))
@@ -28,8 +28,8 @@
        (setf user-mail-address email)
        (setf smtpmail-stream-type 'starttls))))
 
-(defun gnus-gen-dot-gnus (email smtp-server-port imap-server-port)
-  (let ((form (gnus-imap-smtp-form email smtp-server-port imap-server-port))
+(cl-defun gnus-gen-dot-gnus (email &key smtp imap)
+  (let ((form (gnus-imap-smtp-form email smtp imap))
 	(dot-gnus (f-expand "~/.gnus")))
     (unless (or (not (file-exists-p dot-gnus))
 		(when (y-or-n-p "~/.gnus file exists. overwrite?")
@@ -40,7 +40,9 @@
     (message "wrote to %s" dot-gnus)))
 
 
-;(gnus-gen-dot-gnus "erjoalgo@gmail.com" '("smtp.gmail.com" . 587) '("imap.gmail.com" . 993))
+'(gnus-gen-dot-gnus "erjoalgo@gmail.com"
+		    :smtp '("smtp.gmail.com" . 587)
+		    :imap '("imap.gmail.com" . 993))
 
 
 
