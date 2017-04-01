a9fed44bfbdbace69a11c953749b5e9ccb86c490
Ernesto Alfonso <erjoalgo@gmail.com>
2017-04-01 16:58:43 -0700

    add bbdb setup
    not quite what I wanted. bbdb only records
    addresses from messages that are opened.
    ideally, bbdb should create address records
    for any message which passes through the
    summary buffer

---
+++ lisp/settings/bbdb-settings.el
@@ -0,0 +1,9 @@
+(require 'bbdb)
+(bbdb-initialize 'gnus 'message)
+(add-hook 'gnus-startup-hook 'bbdb-insinuate-gnus)
+(bbdb-insinuate-message)
+
+(bbdb-mua-auto-update-init 'gnus 'message)
+(setq bbdb-update-records-p 'create)
+
+(add-hook 'message-setup-hook 'bbdb-mail-aliases)
