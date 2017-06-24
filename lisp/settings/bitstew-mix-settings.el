f81775b50aa3f88fa5bfd56bfed24272ad4acfe7
vagrant <ernesto.alfonsogonzalez@ge.com>
2017-06-24 15:46:52 -0700

    new file:   lisp/settings/bitstew-mix-settings.el
    add mix-language-specific settings

---
+++ lisp/settings/bitstew-mix-settings.el
@@ -0,0 +1,19 @@
+
+(push
+ (buffer-major-mode-matcher
+  'nxml-mode
+  (lambda ()
+    (let* ((director-services-url
+	    "https://192.168.24.10:18443/director-services")
+	   (run-template-prefix
+	    ;; "/SystemServices/main?system:runTemplate="
+	    (concat director-services-url "/SystemServices/main?system:runDebugger="))
+	   (url
+	    (->> (buffer-file-name)
+		 sanitize-filename
+		 (concat run-template-prefix))))
+      (firefox-new-tab url))))
+ erjoalgo-compile-cmd-for-buffer)
+
+(add-to-list 'auto-mode-alist '("[.]mxh$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("[.]mxd$" . nxml-mode))
