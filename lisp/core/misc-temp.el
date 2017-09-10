0bd0abbbfb20bb30bb64d8476cdf6ec03fdbb01f
Ernesto Alfonso <erjoalgo@gmail.com>
2017-09-09 22:30:42 -0700

    misc-temp.el: indent

--- lisp/core/misc-temp.el
+++ lisp/core/misc-temp.el
@@ -2,23 +2,19 @@
 
 (add-hook 'lisp-mode-hook 'slime-mode)
 
-;(add-hook 'shell-script-mode-hook 'bash_install_buttons)
-
-
-
 (defun upcase-last ()
   (interactive)
   (save-excursion
     (upcase-region (point)
-		 (progn
-		   (backward-sexp)
-		   (point)))))
+		   (progn
+		     (backward-sexp)
+		     (point)))))
 
 (defun bash-identifier-current-line  ()
   (let ((line
-	(buffer-substring-no-properties
-	 (point)
-	 (line-beginning-position))))
+	 (buffer-substring-no-properties
+	  (point)
+	  (line-beginning-position))))
     (and
      (string-match
       "^[[:space:]]*\\([^=]+\\)="
@@ -31,25 +27,25 @@
   "a wrapper on top of a process filter function which ensures
 the original filter function is called exactly once for each complete line
 of process output. the return value is a new process-filter function"
-	 (let ((cum-string-sym (gensym "proc-filter-buff"))
+  (let ((cum-string-sym (gensym "proc-filter-buff"))
 	(newline (or separator-char (string-to-char "\n")))
-	       (string-indexof (lambda (string char start)
-				 (loop for i from start below (length string)
+	(string-indexof (lambda (string char start)
+			  (loop for i from start below (length string)
 				thereis (when (eq char (aref string i))
-						    i)))))
-	   (set cum-string-sym "")
-	   `(lambda (proc string)
-	      (setf string (concat ,cum-string-sym string))
-	      (let ((start 0) new-start)
-		(while (setf new-start
-				(funcall ,string-indexof string ,newline start))
+					  i)))))
+    (set cum-string-sym "")
+    `(lambda (proc string)
+       (setf string (concat ,cum-string-sym string))
+       (let ((start 0) new-start)
+	 (while (setf new-start
+		      (funcall ,string-indexof string ,newline start))
 
-		  ;;does not include newline
-		  (funcall ,real-filter proc (substring string start new-start))
+	   ;;does not include newline
+	   (funcall ,real-filter proc (substring string start new-start))
 
-		  (setf start (1+ new-start)));;past newline
+	   (setf start (1+ new-start)));;past newline
 
-		(setf ,cum-string-sym (substring string start))))))
+	 (setf ,cum-string-sym (substring string start))))))
 
 
 
