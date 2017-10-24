4862b78127864a0f18b194364dba96a2e696eab2
Ernesto Alfonso <erjoalgo@gmail.com>
2017-10-24 10:59:47 -0700

    use only date, time and timezone in attachment dirname

--- lisp/settings/gnus-settings.el
+++ lisp/settings/gnus-settings.el
@@ -200,11 +200,12 @@
 
 (defun gnus-dir-name-for-message ()
   (let* ((from (message-fetch-field "From"))
-	(date (message-fetch-field "Date"))
-	(from-nw (gnus-replace-in-string from ".*<\\(.*\\)>.*" "\\1"))
-	(date-nw (-> date
-		     (gnus-replace-in-string "^.*?, \\(.*\\) [+].*" "\\1")
-		     (gnus-replace-in-string " " "-"))))
+	 (date (message-fetch-field "Date"))
+	 (from-nw (gnus-replace-in-string from ".*<\\(.*\\)>.*" "\\1"))
+	 (date-nw (-> date
+		      (gnus-replace-in-string "^.*?, \\(.*? [^(]*\\).*" "\\1")
+		      s-trim
+		      (gnus-replace-in-string "[ :]" "-"))))
     (f-join gnus-attachments-top from-nw date-nw)))
 
 
