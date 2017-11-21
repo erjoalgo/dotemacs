67cec8873cbb11a9783b63e1e7a966824f9f1a10
Ernesto Alfonso <erjoalgo@gmail.com>
2017-11-21 10:29:10 -0800

    merge duplication of *file-programs*
    should be loaded independently of dired mode

--- lisp/extra/file-openers.el
+++ lisp/extra/file-openers.el
@@ -1,14 +1,25 @@
 (setf *file-programs*
   '(
    ("zathura" "pdf" )
-   ("libreoffice" "doc" "ppt" "odt" "docx"  "pptx")
+   ("libreoffice" "doc" "ppt" "odt" "docx"  "pptx" "xls" "ods")
    ("evince" "ps" "eps")
    ("eog" "png" "pgm" "tif" "jpg" "jpeg" "gif")
    ("aplay" "wav")
-   ("firefox" "html")
+   ((lambda (fn)
+      (firefox-new-tab (format "file://%s" fn))) "html")
+   ("gimp" "xcf")
    ("vlc" "mp4" )))
 
 (defun get-file-program (fn)
   (let ((ext (downcase (file-name-extension fn))))
     (loop for (program . exts) in *file-programs* thereis
 	  (and (member ext exts) program))))
+
+(defun open-file (fn)
+  (interactive (list (dired-file-name-at-point)))
+  (setf fn (expand-file-name fn))
+  (let ((program (get-file-program fn)))
+    (if (not program)
+	(error (concat "no program known for file: " fn))
+      (if (functionp program) (funcall program fn)
+	(start-process program nil program fn)))))
--- lisp/settings/dired-settings.el
+++ lisp/settings/dired-settings.el
@@ -1,29 +1,3 @@
-(setf *file-programs*
-  '(
-   ("zathura" "pdf" )
-   ("libreoffice" "doc" "ppt" "odt" "docx"  "pptx" "xls" "ods")
-   ("evince" "ps" "eps")
-   ("eog" "png" "pgm" "tif" "jpg" "jpeg")
-   ("aplay" "wav")
-   ((lambda (fn)
-      (firefox-new-tab (format "file://%s" fn))) "html")
-   ("gimp" "xcf")
-   ("vlc" "mp4" )))
-
-(defun get-file-program (fn)
-  (let ((ext (downcase (file-name-extension fn))))
-    (loop for (program . exts) in *file-programs* thereis
-	  (and (member ext exts) program))))
-
-(defun open-file (fn)
-  (interactive (list (dired-file-name-at-point)))
-  (setf fn (expand-file-name fn))
-  (let ((program (get-file-program fn)))
-    (if (not program)
-	(error (concat "no program known for file: " fn))
-      (if (functionp program) (funcall program fn)
-	(start-process program nil program fn)))))
-
 (with-eval-after-load "dired"
   (define-key dired-mode-map "q" 'dired-up-directory)
   (define-key dired-mode-map (kbd "s-f") 'open-file)
