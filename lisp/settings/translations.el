6348665536ec508b62f31e44d4eb1576c140d57e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-06-14 10:39:02 -0700

    also replace single-quotes not occurring in pairs

--- lisp/settings/translations.el
+++ lisp/settings/translations.el
@@ -269,6 +269,7 @@ a translation from scratch"
                                        (error "unbalanced quotes in line %s"
                                               (line-number-at-pos (match-beginning 0)))))
                                ("'\\(.*?\\)'" "‘\\1’")
+                               ("'" "’")
                                (" *-- *" "—")
                                ("\\([\"”]\\)\\([.,:]\\)" "\\2\\1")
                                ("[.]\\{3\\}" "…")
