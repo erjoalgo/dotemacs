a371aeaebc44786b158484118f60b9494e8d97be
Ernesto Alfonso <erjoalgo@gmail.com>
2018-07-04 20:15:58 -0700

    add dired tagger

--- lisp/settings/dired-settings.el
+++ lisp/settings/dired-settings.el
@@ -28,4 +28,50 @@
 		(temp-buffer-window-show du-buffer)
 		(goto-char (point-max))))
 
+(setf dired-tmp-char 31)
+(setf dired-mark-char 42)
+
+(defun dired-mark-file-as (dest-char)
+  (interactive "center lowercase char, q to quit: ")
+  (unless (member dest-char '(Quit 113))
+    (dired-change-marks dired-mark-char dired-tmp-char)
+    (dired-mark nil)
+    (dired-change-marks dired-mark-char dest-char)
+    (dired-change-marks dired-tmp-char dired-mark-char)
+    t))
+
+(defun dired-tagger-tag-loop ()
+  (interactive)
+  (loop while (call-interactively 'dired-mark-file-as)))
+
+(defun dired-tagger-move ()
+  (interactive)
+
+  (let* ((arr (make-vector 256 nil)))
+
+    (loop for (file . mark) in (dired-remember-marks (point-min) (point-max))
+          do (assert mark)
+          if (eq mark dired-mark-char) do
+          (error "remove the standard mark from all files first: %s"
+                 file)
+          else do
+          (push file (aref arr mark)))
+
+    (loop for files across arr
+          for mark from 0
+          if files do
+          (progn (message "mark: %s: %s" mark files)
+                 '(let ((dest
+                         (find-file ))))
+                 (message "files '%s' are: %s"
+                          (char-to-string mark)
+                          (s-join "\n" files))
+                 (dired-change-marks mark dired-mark-char)
+                 (dired-do-rename)))))
+
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map (kbd "b") 'dired-mark-file-as)
+     (define-key dired-mode-map (kbd "B") 'dired-tagger-tag-loop)))
+
 (setq image-dired-show-all-from-dir-max-files 2000)
