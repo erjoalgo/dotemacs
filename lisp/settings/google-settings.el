69859f9510067424951b652eef14f1153784e6bc
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-10 21:13:07 -0700

    google emacs libraries setup

--- lisp/settings/google-settings.el
+++ lisp/settings/google-settings.el
@@ -3,4 +3,12 @@
 (push `("BUILD" . python-mode) auto-mode-alist)
 
 ;; don't use chromium
+
+(require 'google)
+(require 'google3-build)
+(require 'csearch)
 (setf browser-name (car (which "chrome" "google-chrome")))
+
+
+(require 'google3-language-services)
+(google3-language-services-setup)
