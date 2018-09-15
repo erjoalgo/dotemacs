8650493c1e1a7be46bcee7eae46e62a67ba5e16e
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-15 00:52:17 -0700

    use hook to always keep mode line updated

--- lisp/settings/mode-line.el
+++ lisp/settings/mode-line.el
@@ -1,15 +1,24 @@
-(put 'org-mode-line-string 'risky-local-variable nil)
+(defvar erjoalgo-mode-line-format
+  '("%e"
+    mode-line-front-space
+    mode-line-mule-info
+    mode-line-client
+    mode-line-modified
+    mode-line-remote
+    mode-line-frame-identification
+    mode-line-buffer-identification
+    " "
+    mode-line-position
+    (:propertize mode-line-misc-info 0 32)
+    mode-line-end-spaces
+    ""))
 
-(setf mode-line-format
-      '("%e"
-        mode-line-front-space
-        mode-line-mule-info
-        mode-line-client
-        mode-line-modified
-        mode-line-remote
-        mode-line-frame-identification
-        mode-line-buffer-identification
-        mode-line-position
-        (:propertize mode-line-misc-info 0 32)
-        mode-line-end-spaces
-        ""))
+(defun mode-line-update-erjoalgo ()
+  (put 'org-mode-line-string 'risky-local-variable nil)
+  (setf mode-line-format erjoalgo-mode-line-format))
+
+;; setting the value once does not seem to be enough...
+(add-hook
+ ;; 'change-major-mode-hook
+ 'after-change-major-mode-hook
+ 'mode-line-update-erjoalgo)
