971d1c0446b44709e868fa0891057fa46d06ffdb
Ernesto Alfonso <erjoalgo@gmail.com>
2018-09-16 11:03:19 -0700

    don't err if google library not available

--- lisp/settings/google-settings.el
+++ lisp/settings/google-settings.el
@@ -1,14 +1,13 @@
-;; TODO find appropriate modes
-(push `("[.]sdl" . sql-mode) auto-mode-alist)
-(push `("BUILD" . python-mode) auto-mode-alist)
+(when (require 'google nil t)
 
-;; don't use chromium
+  ;; (push `("[.]sdl" . sql-mode) auto-mode-alist)
+  ;; (push `("BUILD" . python-mode) auto-mode-alist)
 
-(require 'google)
-(require 'google3-build)
-(require 'csearch)
-(setf browser-name (car (which "chrome" "google-chrome")))
+  (require 'google3-build)
+  (require 'csearch)
+  ;; don't use chromium
+  (setf browser-name (car (which "chrome" "google-chrome")))
 
 
-(require 'google3-language-services)
-(google3-language-services-setup)
+  (require 'google3-language-services)
+  (google3-language-services-setup))
