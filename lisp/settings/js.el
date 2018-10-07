39f75ce17650c6fc5d6dada040c47027093dbb5b
Ernesto Alfonso <erjoalgo@gmail.com>
2018-10-07 03:01:05 -0700

    add flymake js config and tmp workaround

---
+++ lisp/settings/js.el
@@ -0,0 +1,8 @@
+;; (require 'flymake-jslint) ;; Not necessary if using ELPA package
+
+(require 'flymake-jslint)
+(setf jsl-err-line-pattern
+      '("^\\(.*?\\)(\\([0-9]+\\)): \\(.*\\)" 1 2 nil 3))
+(add-hook 'js-mode-hook 'flymake-jslint-load)
+
+(push jsl-err-line-pattern flymake-proc-err-line-patterns)
