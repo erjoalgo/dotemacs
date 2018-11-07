c822ddb7b226eb7726f78b9bc7ba5bf9fee599c5
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-06 21:59:14 -0800

    move google out settings

--- lisp/settings/google-settings.el
+++ lisp/settings/google-settings.el
@@ -1,14 +1,1 @@
-(when (require 'google nil t)
-  ;; TODO find appropriate modes
-  (push `(".pkgdef$" . borg-mode) auto-mode-alist)
-  ;; (push `("[.]sdl" . sql-mode) auto-mode-alist)
-  ;; (push `("BUILD" . python-mode) auto-mode-alist)
 
-  (require 'google3-build)
-  (require 'csearch)
-  ;; don't use chromium
-  (setf browser-name (car (which "chrome" "google-chrome")))
-
-
-  (require 'google3-language-services)
-  (google3-language-services-setup))
