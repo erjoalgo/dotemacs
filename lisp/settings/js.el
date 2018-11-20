a9a40bda4f8151f8497bf4aabd868c892b098f7a
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-20 00:57:35 -0800

    remove explicit require

--- lisp/settings/js.el
+++ lisp/settings/js.el
@@ -1,6 +1,5 @@
 ;; (require 'flymake-jslint) ;; Not necessary if using ELPA package
 
-(require 'flymake-jslint)
 (setf jsl-err-line-pattern
       '("^\\(.*?\\)(\\([0-9]+\\)): \\(.*\\)" 1 2 nil 3))
 (add-hook 'js-mode-hook 'flymake-jslint-load)
