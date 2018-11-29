5cef7acaf282eca9b4c3cc9d61ca8889c178fe57
Ernesto Alfonso <erjoalgo@gmail.com>
2018-11-28 17:20:56 -0800

    add cpp-sort-includes to before-save-hook

--- lisp/settings/cpp.el
+++ lisp/settings/cpp.el
@@ -1,7 +1,11 @@
 (defun cpp-sort-includes ()
   (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward "\\(^#include.*\n\\)+" nil t)
-      (sort-lines nil (match-beginning 0)
-                  (match-end 0)))))
+  (when (eq major-mode 'c++-mode)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "\\(^#include.*\n\\)+" nil t)
+        (sort-lines nil (match-beginning 0)
+                    (match-end 0))))))
+
+
+(add-hook 'before-save-hook 'cpp-sort-includes)
