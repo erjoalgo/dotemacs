c2a5abcf429a615d2f7634967274fb836987c62f
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-11 22:28:49 -0800

    address flycheck feedback, add test

--- lisp/core/genpass.el
+++ lisp/core/genpass.el
@@ -1,4 +1,4 @@
-;;; genpass.el ---
+;;; genpass.el --- Generate a password
 
 ;; Copyright (C) 2016  Ernesto Alfonso <erjoalgo@gmail.com>
 
@@ -22,50 +22,69 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+(require 'seq)
+(require 'select)
 
 (defun genpass-ranges-to-bag (ranges)
-  (assert (evenp (length ranges)))
-  (loop with bag = ""
-   for i below (length ranges) by 2
-	as a = (aref ranges i)
-	as b = (aref ranges (1+ i))
-	as str = (let ((b (max a b)) (a (min a b)) len str)
-		   (setf len (1+ (- b a)))
-		   (setf str (make-string len 0))
-		   (loop for c from a upto b
-			 for i from 0 do
-			 (aset str i c))
-		   str)
-	do (setf bag (concat bag str))
-	finally (return bag)))
+  "Expand character ranges, specified as start-end character pairs in the string RANGES."
+  (cl-assert (cl-evenp (length ranges)))
+  (cl-loop for i below (length ranges) by 2
+	   as a = (aref ranges i)
+	   as b = (aref ranges (1+ i))
+	   concat (let* ((b (max a b))
+                         (a (min a b))
+                         (len (1+ (- b a)))
+                         (str (make-string len 0)))
+		    (cl-loop for c from a upto b
+		             for i from 0 do
+		             (aset str i c))
+		    str)))
 
-;(defvar *genpass-alnum* (genpass-ranges-to-bag "azAZ09"))
-(defvar *genpass-letters-lower* (genpass-ranges-to-bag "az"))
-(defvar *genpass-letters-upper* (genpass-ranges-to-bag "AZ"))
-(defvar *genpass-letters* (concat *genpass-letters-upper* *genpass-letters-lower*))
-;(defvar *genpass-letters* (genpass-ranges-to-bag "azAZ"))
-(defvar *genpass-num* (genpass-ranges-to-bag "09"))
-(defvar *genpass-alnum* (concat *genpass-num* *genpass-letters*))
-(defvar *genpass-special-chars* (genpass-ranges-to-bag "!/:@"))
-(defvar *genpass-all* (concat *genpass-special-chars* *genpass-alnum*))
-(defvar *genpass-default-len* 13)
+;(defvar genpass-alnum (genpass-ranges-to-bag "azAZ09"))
+(defvar genpass-letters-lower (genpass-ranges-to-bag "az"))
+(defvar genpass-letters-upper (genpass-ranges-to-bag "AZ"))
+(defvar genpass-letters (concat genpass-letters-upper genpass-letters-lower))
+;(defvar genpass-letters (genpass-ranges-to-bag "azAZ"))
+(defvar genpass-num (genpass-ranges-to-bag "09"))
+(defvar genpass-alnum (concat genpass-num genpass-letters))
+(defvar genpass-special-chars (genpass-ranges-to-bag "!/:@"))
+(defvar genpass-all (concat genpass-special-chars genpass-alnum))
+(defvar genpass-default-len 13)
+
+(defun genpass-set-clipboard (text)
+  "Set the clipboard to the string TEXT."
+  (gui-set-selection 'CLIPBOARD text)
+  (gui-set-selection 'PRIMARY text))
 
 (defun genpass-genpass (n bag)
-  ;;(interactive "npassword length: ")
+  "Generate a passsword of length N using char-bag BAG."
   (interactive (list (if current-prefix-arg
 			 (read-number "password length: ")
-		       *genpass-default-len*)
-		     *genpass-alnum*))
-  (loop with str = (make-string n 0)
-	with bag-len = (length bag)
-	for i below n do
-	(aset str i (aref bag (random bag-len)))
-	finally (progn (message str)
-		       (if window-system
-			   (set-clipboard str)
-			 (kill-new str))
-		       (return str))))
+		       genpass-default-len)
+		     genpass-special-chars))
+  (cl-loop with str = (make-string n 0)
+	   for i below n do
+	   (aset str i (seq-random-elt bag))
+	   finally (progn (message str)
+		          (if window-system
+			      (genpass-set-clipboard str)
+			    (kill-new str))
+		          (cl-return str))))
+
+(ert-deftest test-genpass ()
+  (cl-loop for n below 20 do
+           (cl-loop for (fmt bag)
+                    on (list
+                        "[a-z]" genpass-letters-lower
+                        "[A-Z]" genpass-letters-upper
+                        "[a-zA-Z]" genpass-letters
+                        "[0-9]" genpass-num
+                        "[a-zA-Z0-9]" genpass-num)
+                    by #'cddr
+                    as regexp = (format "%s\\{%d\\}" fmt n) do
+                    (should
+                     (string-match regexp (genpass-genpass n bag))))))
 
 (provide 'genpass)
 ;;; genpass.el ends here
-
