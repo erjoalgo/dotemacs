ab61a66948b73b364959aa6f26ad48a044185de0
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-17 00:16:13 -0800

    flycheck

--- lisp/core/isearch-fast-reverse.el
+++ lisp/core/isearch-fast-reverse.el
@@ -1,4 +1,4 @@
-;;; isearch-fast-reverse.el --- 
+;;; isearch-fast-reverse.el --- Make it easier to reverse isearch direction
 
 ;; Copyright (C) 2016  Ernesto Alfonso <erjoalgo@gmail.com>
 
@@ -19,15 +19,16 @@
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;Make switching directions in isearch mode faster.
-;;Changes:
-;;Make regexp search the default search
-;;Reverse directions and get next match in a single keystroke.
-;;Bind forward search, reverse search to f3, M-f3
-;;Esc to exit isearch at current position
-;;C-g or f4 to cancel isearch and return to original, pre-search position
-;;Exiting while on isearch-forward will move point to beginning of match, not at the end. This makes it easier to move point where desired via isearch-forward
-;;In dired or gnus, RET automatically opens file/directory at point
+;;;  Make switching directions in isearch mode faster.
+;;;  Changes:
+;;;  Make regexp search the default search
+;;;  Reverse directions and get next match in a single keystroke.
+;;;  Bind forward search, reverse search to f3, M-f3
+;;;  Esc to exit isearch at current position
+;;;  C-g or f4 to cancel isearch and return to original, pre-search position
+;;;  Exiting while on isearch-forward will move point to beginning of match, not at the end.
+;;;  This makes it easier to move point where desired via isearch-forward
+;;;  In dired or gnus, RET automatically opens file/directory at point
 
 
 ;;; Code:
@@ -39,52 +40,60 @@
 (global-set-key [f3] 'isearch-forward-regexp)
 ;should work because it is a recedit
 (global-set-key [M-f3] (lambda () (interactive)
-			 (let* ((isearch-reverse-direction-p t))
+			 (let* ((isearch-backwards-p t))
 			   (isearch-backward-regexp))))
 
 
-(defvar isearch-reverse-direction-p nil)
+(defvar isearch-backwards-p nil)
 
 (defun isearch-forward-or-backward ()
+  "Do an isearch either forward or backward based on ISEARCH-BACKWARDS-P."
   (interactive)
   (let* ((case-fold-search t))
-    (if isearch-reverse-direction-p (isearch-repeat-backward)
+    (if isearch-backwards-p (isearch-repeat-backward)
       (isearch-repeat-forward))))
 
 
 (defun isearch-reverse-search-and-search ()
+  "Reverse isearch direction and search."
   (interactive)
-  (setq isearch-reverse-direction-p (not isearch-reverse-direction-p))
+  (setq isearch-backwards-p (not isearch-backwards-p))
   (isearch-forward-or-backward)
   (isearch-forward-or-backward))
 
 
 (add-hook 'isearch-mode-end-hook
-	  (lambda () (setq isearch-reverse-direction-p nil)))
-
-
-;;TODO turn into hooks?
-
+	  (lambda ()
+	    "Clean up ‘isearch-backwards-p' state."
+	    (setq isearch-backwards-p nil)))
 
 (defadvice isearch-forward-regexp (around force-case-fold activate)
+  "Force isearch case fold always."
   (let* ((case-fold-search t))
     (when current-prefix-arg
       (set-mark-command nil))
     ad-do-it))
 
-
 (defadvice isearch-reverse-exit (after dired-search-maybe-follow activate)
+  "Maybe re-issue the RET key used to stop an isearch onto the major mode map."
   (isearch-ret-maybe-passthrough))
 
 (defadvice isearch-exit (before dired-search-maybe-follow activate)
+  "Maybe re-issue the RET key used to stop an isearch onto the major mode map.
+
+   When exiting isearch foward normally, move point to the begining
+   of the match, not the end.
+   This means point should be left at the begining of a variable or another
+   interesting place for further edit, saving a call to e.g. ‘backward-word'."
   (isearch-ret-maybe-passthrough)
   (when (and isearch-forward isearch-other-end)
     (goto-char isearch-other-end)))
 
 
 (defun isearch-ret-maybe-passthrough ()
+  "Maybe re-issue the RET key used to stop an isearch onto the major mode map."
   (when (member (this-command-keys) '([return] ""))
-    (case major-mode
+    (cl-case major-mode
       ('dired-mode (dired-find-file))
       ('gnus-summary-mode (gnus-summary-scroll-up nil)))))
 
