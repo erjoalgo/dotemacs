03bf88d8802313c3684d79a9d673b727a789e780
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-19 21:09:33 -0800

    fake the slime-helper.el file

--- test/test.sh
+++ test/test.sh
@@ -6,7 +6,9 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 TMPDIR=/tmp/emacs-init-file-test
 # rm -rf "${TMPDIR}"
-mkdir -p "${TMPDIR}"
+# mkdir -p "${TMPDIR}"
+mkdir -p "${TMPDIR}/quicklisp"
+touch "${TMPDIR}/quicklisp/slime-helper.el"
 
 ln -sf $(realpath ../lisp/.emacs) "${TMPDIR}/"
 
