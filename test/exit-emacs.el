ec1bb6ed2a0983d592ec63d7f4b8727d506d603f
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-19 20:43:28 -0800

    print buffer output unconditionally

--- test/exit-emacs.el
+++ test/exit-emacs.el
@@ -1,11 +1,9 @@
 (let ((warnings (get-buffer "*Warnings*")))
-  (if (not warnings)
-      (kill-emacs)
-    (dolist (buff (list (get-buffer "*Messages*") warnings))
-      (when buff
-        (with-current-buffer buff
-          (print (buffer-substring-no-properties
-                  (point-min)
-                  (point-max))
-                 #'external-debugging-output))))
-    (kill-emacs 1)))
+  (dolist (buff (list (get-buffer "*Messages*") warnings))
+    (when buff
+      (with-current-buffer buff
+        (print (buffer-substring-no-properties
+                (point-min)
+                (point-max))
+               #'external-debugging-output))))
+  (kill-emacs (when warnings 1)))
