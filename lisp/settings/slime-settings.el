2593ddf3966f27a2c0b09df5375220e512927db2
Ernesto Alfonso <erjoalgo@gmail.com>
2018-12-19 20:47:13 -0800

    fxp slime fmt

--- lisp/settings/slime-settings.el
+++ lisp/settings/slime-settings.el
@@ -4,7 +4,6 @@
       (expand-file-name "~/quicklisp/slime-helper.el"))
 
 (if (not (file-exists-p slime-helper-filename))
-    ;; must (ql:quickload "quicklisp-slime-helper")
     (warn "slime helper %s does not exist. hint: %s"
 	  slime-helper-filename
 	  '(ql:quickload 'quicklisp-slime-helper))
@@ -12,27 +11,26 @@
   (load slime-helper-filename))
 
 (with-eval-after-load "slime"
-    (setq inferior-lisp-program "sbcl")
+  (setq inferior-lisp-program "sbcl")
+  (setq slime-contribs '(slime-fancy))
+  (require 'slime-autoloads)
+  (add-hook 'lisp-mode-hook 'slime-mode)
 
-    (setq slime-contribs '(slime-fancy))
-    (require 'slime-autoloads)
-    (add-hook 'lisp-mode-hook 'slime-mode)
-
-    (defun find-buffer-by-prefix (prefix)
-      (cl-loop for buff in (buffer-list)
-	    thereis (and (s-starts-with-p
-			  prefix
-			  (buffer-name buff))
-			 buff)))
-
+  (defun find-buffer-by-prefix (prefix)
+    (cl-loop for buff in (buffer-list)
+	     thereis (and (s-starts-with-p
+			   prefix
+			   (buffer-name buff))
+			  buff)))
 
 
-    (add-hook 'sldb-hook 'visual-line-mode)
-    (add-hook 'sldb-hook 'beginning-of-buffer)
-    (define-key slime-repl-mode-map (kbd "s-h") slime-doc-map)
-    (setf slime-load-failed-fasl 'never)
 
-    (defun slime-killall ()
-      (interactive)
-      (kill-buffers-matching-regexp "[*]\\(slime-repl\\|inferior-lisp\\|slime\\)")
-      (slime-disconnect-all)))
+  (add-hook 'sldb-hook 'visual-line-mode)
+  (add-hook 'sldb-hook 'beginning-of-buffer)
+  (define-key slime-repl-mode-map (kbd "s-h") slime-doc-map)
+  (setf slime-load-failed-fasl 'never)
+
+  (defun slime-killall ()
+    (interactive)
+    (kill-buffers-matching-regexp "[*]\\(slime-repl\\|inferior-lisp\\|slime\\)")
+    (slime-disconnect-all)))
