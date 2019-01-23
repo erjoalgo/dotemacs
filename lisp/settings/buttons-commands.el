f878c9d68dd988ca03f5658058d66b659052d1c6
Ernesto Alfonso <erjoalgo@gmail.com>
2019-01-23 00:32:26 -0800

    move new key-bound commands to their own file

---
+++ lisp/settings/buttons-commands.el
@@ -0,0 +1,43 @@
+(defun my-upcase-region (a b &optional downcase)
+  "Upcase or downcase region A, B based on DOWNCASE, falling back to the current sexp."
+  (interactive "r")
+  (unless (region-active-p)
+    (setq a (save-excursion (backward-sexp) (point))
+          b (save-excursion (forward-sexp) (point))))
+  (let ((fn (if downcase 'downcase-region 'upcase-region)))
+    (funcall fn a b)))
+
+(defun my-downcase-region (a b)
+  "(my-upcase-region A B t) ."
+  (interactive "r")
+  (my-upcase-region a b t))
+
+
+(defun my-next-error (&optional prev)
+  (interactive)
+  (call-interactively
+   (cond
+    ((get-buffer "*compilation*") (if prev 'previous-error 'next-error))
+    ((and (bound-and-true-p flycheck-mode)
+          (or (flycheck-next-error-pos 1)
+              (flycheck-next-error-pos -1)))
+     (if prev 'flycheck-previous-error 'flycheck-next-error))
+    ((bound-and-true-p flymake-mode) (if prev 'flymake-goto-prev-error 'flymake-goto-next-error))
+    (t (if prev 'previous-error 'next-error)))))
+
+(defun my-prev-error ()
+  (interactive)
+  (my-next-error t))
+
+(defun git-hunk-toggle-cmd (dest-indicator)
+  `(lambda (a b)
+     ,(format "make region hunk lines start with '%s'" dest-indicator)
+     (interactive (if (region-active-p)
+                      (list (region-beginning) (region-end))
+                    (list (line-beginning-position)
+                          (line-end-position
+                           (when (numberp current-prefix-arg) current-prefix-arg)))))
+     (save-excursion
+       (goto-char a)
+       (while (re-search-forward "^[-+ ]" b t nil)
+         (replace-match ,dest-indicator t)))))
--- lisp/settings/buttons-data.el
+++ lisp/settings/buttons-data.el
@@ -47,21 +47,6 @@
 
 (require 'buttons)
 
-(defun my-upcase-region (a b &optional downcase)
-  "Upcase or downcase region A, B based on DOWNCASE, falling back to the current sexp."
-  (interactive "r")
-  (unless (region-active-p)
-    (setq a (save-excursion (backward-sexp) (point))
-          b (save-excursion (forward-sexp) (point))))
-  (let ((fn (if downcase 'downcase-region 'upcase-region)))
-    (funcall fn a b)))
-
-(defun my-downcase-region (a b)
-  "(my-upcase-region A B t) ."
-  (interactive "r")
-  (my-upcase-region a b t))
-
-
 (buttons-macrolet
  ((inm () (when (functionp 'global-erjoalgo-command-mode)
             `(global-erjoalgo-command-mode 0)))
@@ -123,22 +108,6 @@
      ("R" (cmd (ins "***REMOVED***")))
      ;; ([?\s-\t] #'company-complete)
      ((kbd "<s-tab>") #'company-complete)))
-
- (defun my-next-error (&optional prev)
-   (interactive)
-   (call-interactively
-    (cond
-     ((get-buffer "*compilation*") (if prev 'previous-error 'next-error))
-     ((and (bound-and-true-p flycheck-mode)
-           (or (flycheck-next-error-pos 1)
-               (flycheck-next-error-pos -1)))
-      (if prev 'flycheck-previous-error 'flycheck-next-error))
-     ((bound-and-true-p flymake-mode) (if prev 'flymake-goto-prev-error 'flymake-goto-next-error))
-     (t (if prev 'previous-error 'next-error)))))
-
- (defun my-prev-error ()
-   (interactive)
-   (my-next-error t))
 
  (defbuttons python-buttons programming-buttons
    (python-mode-map)
@@ -1125,19 +1094,6 @@
    (but
     ("p" 'magit-go-backward)
     ("n" 'magit-go-forward)))
-
- (defun git-hunk-toggle-cmd (dest-indicator)
-   `(lambda (a b)
-      ,(format "make region hunk lines start with '%s'" dest-indicator)
-      (interactive (if (region-active-p)
-                       (list (region-beginning) (region-end))
-                     (list (line-beginning-position)
-                           (line-end-position
-                            (when (numberp current-prefix-arg) current-prefix-arg)))))
-      (save-excursion
-        (goto-char a)
-        (while (re-search-forward "^[-+ ]" b t nil)
-          (replace-match ,dest-indicator t)))))
 
  (defbuttons diff-buttons nil
    (diff-mode-map)
