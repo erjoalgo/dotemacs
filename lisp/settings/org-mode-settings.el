ec6c5299bd1df44d4a8976a4c80ba7346ec43f42
Ernesto Alfonso <erjoalgo@gmail.com>
2019-03-14 22:02:38 -0700

    import code to support embedded youtube videos
    http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html

--- lisp/settings/org-mode-settings.el
+++ lisp/settings/org-mode-settings.el
@@ -283,3 +283,25 @@
                              "]]")))))
 
 
+(defvar yt-iframe-format
+  ;; http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ ;; http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
