3fcf3fb42e599d0af4d3c52df63b3696dce07f11
Ernesto Alfonso <erjoalgo@gmail.com>
2019-04-21 17:47:53 -0700

    fix pdf autobuild rule

--- lisp/settings/autobuild-settings.el
+++ lisp/settings/autobuild-settings.el
@@ -1,6 +1,6 @@
-(autobuild-define-rule autobuild-pdf-zathura (fundamental-mode)
+(autobuild-define-rule autobuild-pdf-zathura (fundamental-mode doc-view-mode)
   (autobuild-nice 9)
-  (when (and buffer-file-name (f-ext buffer-file-name))
+  (when (and buffer-file-name (equal "pdf" (f-ext buffer-file-name)))
     (format "zathura ./%s"
             (-> buffer-file-name
               f-filename
