2043643198f94a091ee4379fa21a6db9a11e516e
Ernesto Alfonso <erjoalgo@gmail.com>
2019-06-12 20:04:09 -0700

    include 1962 in x-service default ports

--- lisp/settings/stupmwm.el
+++ lisp/settings/stupmwm.el
@@ -49,7 +49,7 @@ in the current STUMPWM group/workspace."
 (defun stumpwm-request (path &optional host ports)
   (let* ((host (or host "localhost"))
          (ports (or (if (numberp ports) (list ports) ports)
-                    '(1959 1960 1961))))
+                    '(1959 1960 1961 1962))))
     (loop for port in ports
           as url = (format "http://%s:%s%s" host port path)
           do (url-retrieve
