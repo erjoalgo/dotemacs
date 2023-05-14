a5efa1ccfc84f3a871987128487db44a1aac6699
Ernesto Alfonso <erjoalgo@gmail.com>
2023-05-14 17:49:24 -0400

    fix removed (gnus) call

--- lisp/settings/gnus-settings.el
+++ lisp/settings/gnus-settings.el
@@ -81,6 +81,7 @@
       '(unless (or (gnus))
         ;; Unable to open server nnimap+gmail.com due to: auth-source-netrc-parse-entries: Unexpected ‘machine’ token at line 3
         (error "we appear to have failed to connect via gnus"))
+      (gnus)
       ;; (gnus-group-read-group 5000 t sent-group-name )
       (gnus-group-read-group 1000 t inbox)
       (unless (byte-code-function-p (symbol-function 'gnus))
