9bf2c5103dae2d2805d9c1fb7a2433b1dfd8c8dd
Ernesto Alfonso <erjoalgo@gmail.com>
2023-06-28 12:42:53 -0400

    support leetcode-insert-test-case

--- lisp/settings/leetcode.el
+++ lisp/settings/leetcode.el
@@ -1,20 +1,88 @@
+(defun leetcode-find-solution-name ()
+  (save-excursion
+    (goto-char (point-min))
+    (save-match-data
+      (cl-assert (re-search-forward "^public:\n +.*? +\\([^ (]+\\)("))
+      (match-string 1))))
+
+(defun leetcode-insert-test-case (text)
+  (interactive "senter leetcode testcase text: ")
+  (let ((fn-name (leetcode-find-solution-name)))
+    (buttons-macrolet nil
+      (indent-for-tab-command)
+      (ins "{") (nli)
+      (ins "auto res = sol." fn-name "(")
+      (leetcode-test-case-to-c++ text)
+      (ins ");") (nli)
+      (ins "cout << \"result: \" << res << endl;") (nli)
+      (ins "for (const int resi : res) {") (nli)
+      (ins "cout << \"result: \" << resi << endl;") (nli)
+      (ins "}")  (indent-for-tab-command) (nli)
+      (ins "}") (indent-for-tab-command) (nli))))
+
 (defun leetcode-insert-c++-main ()
   (interactive)
-  (insert "int main (int argc, char* argv[]) {")
-  (newline-and-indent)
-  (insert "Solution sol;")
-  (newline-and-indent)
-  (let ((fn-name
-         (save-excursion
-           (goto-char (point-min))
-           (save-match-data
-             (cl-assert (re-search-forward "^public:\n +[^ ]+ +\\([^ (]+\\)"))
-             (match-string 1)))))
-    (insert "auto res = sol." fn-name "(")
-    (recursive-edit)
-    (insert ");")
-    (newline-and-indent))
-  (insert "cout << \"result: \" << res << endl;")
-  (newline-and-indent)
-  (insert "}")
-  (indent-for-tab-command))
+  (cpp-maybe-insert-includes)
+  (goto-char (point-max))
+  (buttons-macrolet nil
+    (nli)
+    (ins "int main (int argc, char* argv[]) {") (nli)
+    (ins "Solution sol;") (nli)
+    (leetcode-insert-test-case (read-string "enter test case: " (x-get-selection)))
+    (ins "}")
+    (indent-for-tab-command)))
+
+(defun leetcode-test-case-to-c++ (text)
+  (interactive (list (x-get-selection)))
+  (setq text (string-trim-left text "Input: "))
+  (setq text (string-trim-right text "\n"))
+  (cl-loop
+   with defs
+   with args
+   for var in (split-string text ", \\|\n")
+   for i from 1
+   as kv = (split-string var " = ")
+   do (cl-destructuring-bind (k v)
+          (if (cdr kv) kv (list (format "var%s" i) (car kv)))
+        (if (s-contains-p "]" v)
+            (progn
+              (when (> (length v) 10000)
+                (message "DDEBUG dk0l (length v): %s" (length v))
+                (setq v (s-replace-all '(("," . ",\n")) v)))
+              (push (format "vector %s = %s;" k
+                            (s-replace "]" "}"
+                                       (s-replace "[" "{" v)))
+                    defs)
+              (push k args))
+          (push v args)))
+   finally
+   (progn
+     (save-excursion
+       (beginning-of-line)
+       (cl-loop for def in (seq-reverse defs)
+                do (indent-for-tab-command)
+                do (insert def)
+                do (newline-and-indent)))
+     (insert (string-join (seq-reverse args) ", ")))))
+
+
+(defun cpp-maybe-insert-includes ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (unless (save-excursion (re-search-forward "^using namespace std" nil t))
+      (buttons-macrolet nil
+       (ins "#include <algorithm>") (nli)
+       (ins "#include <array>") (nli)
+       (ins "#include <assert.h>") (nli)
+       (ins "#include <cmath>") (nli)
+       (ins "#include <iostream>") (nli)
+       (ins "#include <map>") (nli)
+       (ins "#include <queue>") (nli)
+       (ins "#include <set>") (nli)
+       (ins "#include <string>") (nli)
+       (ins "#include <unordered_map>") (nli)
+       (ins "#include <unordered_set>") (nli)
+       (ins "#include <vector>") (nli)
+       (nli)
+       (ins "using namespace std;") (nli)))))
