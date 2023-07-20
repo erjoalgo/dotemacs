1f06ea6bf2c1983426b7cf7778454a38f4fbe9d8
Ernesto Alfonso <erjoalgo@gmail.com>
2023-07-20 15:06:47 -0400

    refactor file-openers

--- lisp/extra/file-openers.el
+++ lisp/extra/file-openers.el
@@ -1,18 +1,34 @@
-(setf *file-programs*
-  '(
-   ("zathura" . ("pdf"))
-   ("libreoffice" . ("doc" "ppt" "odt" "docx"  "pptx" "xls" "ods"))
-   ("evince" . ("ps" "eps"))
-   ("eog" . ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg"))
-   ("aplay" "wav")
-   ("timidity" . ("midi" "mid"))
-   ((lambda (fn)
-      (browser-new-tab (format "file://%s" fn))) "html")
-   ("gimp" . ("xcf"))
-   ("mpg321" . ("mp3"))
-   ("vlc" . ("mov" "mkv" "aac"))
-   ("celluloid" . ("mp4"))
-   ("xournal" . ("xoj"))))
+(require 'cl-lib)
+
+(defmacro def-open-file-program (opener exts)
+  "Specify that file extensions in EXTS should be opened by OPENER.
+
+   OPENER can be a program name or a function."
+  `(progn
+     (setq *file-programs*
+           (cl-remove ,opener *file-programs*
+                      :key #'car :test #'equal))
+     (push (cons ,opener ',exts) *file-programs*)))
+
+(defvar *file-programs* nil "Alist of (PROGRAM . EXTENSIONS) pairs")
+
+(def-open-file-program "zathura" ("pdf"))
+(def-open-file-program "libreoffice" ("doc" "ppt" "odt" "docx"  "pptx" "xls" "ods"))
+(def-open-file-program "evince" ("ps" "eps"))
+(def-open-file-program "eog" ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg"))
+(def-open-file-program "aplay" ("wav"))
+(def-open-file-program "timidity" ("midi" "mid"))
+(defun open-html-file (filename)
+  (lambda (filename)
+    (browser-new-tab (format "file://%s" filename))))
+(def-open-file-program #'open-html-file ("html"))
+(def-open-file-program #'find-file ("txt"))
+(def-open-file-program "gimp" ("xcf"))
+(def-open-file-program "mpg321" ("mp3"))
+(def-open-file-program "vlc" ("mov" "mkv" "aac"))
+(def-open-file-program "celluloid" ("mp4"))
+(def-open-file-program "xournal" ("xoj"))
+
 
 (defvar open-exe
   (s-trim (shell-command-to-string "which open")))
@@ -20,9 +36,11 @@
 (defun get-file-program (fn)
   (if (equal system-type 'darwin)
       open-exe
-    (let ((ext (downcase (file-name-extension fn))))
-      (cl-loop for (program . exts) in *file-programs* thereis
-            (and (member ext exts) program)))))
+    (let ((ext (file-name-extension fn)))
+      (when ext
+        (setq ext (downcase ext))
+        (cl-loop for (program . exts) in *file-programs* thereis
+                 (and (member ext exts) program))))))
 
 (defun coalesce (&rest strings)
   (cl-loop for s in strings
@@ -36,7 +54,7 @@
   (let ((program (get-file-program filename)))
     (if (not program)
         (progn
-          (warn (concat "no program known for file: " filename))
+          (message (concat "no program known for file: " filename))
           (find-file filename))
       (if (functionp program) (funcall program filename)
 	(progn (message "%s %s" program filename)
