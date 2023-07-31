c8882e6a9f0b2acd4ff4b6f885a054a3644f8017
Ernesto Alfonso <erjoalgo@gmail.com>
2023-07-31 18:20:50 -0400

    add .ico file opener

--- lisp/extra/file-openers.el
+++ lisp/extra/file-openers.el
@@ -15,7 +15,7 @@
 (def-open-file-program "zathura" ("pdf"))
 (def-open-file-program "libreoffice" ("doc" "ppt" "odt" "docx"  "pptx" "xls" "ods"))
 (def-open-file-program "evince" ("ps" "eps"))
-(def-open-file-program "eog" ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg"))
+(def-open-file-program "eog" ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg" "ico"))
 (def-open-file-program "aplay" ("wav"))
 (def-open-file-program "timidity" ("midi" "mid"))
 (defun open-html-file (filename)
