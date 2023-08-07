24a09186acc95212f3a0fe191535acac3d49af79
Ernesto Alfonso <erjoalgo@gmail.com>
2023-08-07 12:06:27 -0400

    add tool to download remote img srcs

--- lisp/settings/js.el
+++ lisp/settings/js.el
@@ -16,3 +16,26 @@
          4)))
 
 (add-hook 'js-mode-hook 'js-autodetect-indent-level)
+
+(defun img-src-graphics-make-local ()
+  (interactive)
+  (let ((dest "./resources"))
+    (unless (file-exists-p dest)
+      (make-directory dest))
+    (save-excursion
+      (goto-char (point-min))
+      (cl-loop while (let ((case-fold-search t))
+                       (re-search-forward "src=\"\\([^\"]+\\(jp?eg\\|png\\|gif\\)\\)\"" nil t))
+               as url = (match-string 1)
+               as base = (f-filename url)
+               as local = (f-join dest base)
+               do (progn
+                    (if (file-exists-p local)
+                      (message "skipping already downloaded file: %s" local)
+                    (call-process
+                     "curl"
+                     nil
+                     (get-buffer-create "*img-src-graphics-make-local*")
+                     t
+                     url "-Lo" local))
+                    (replace-match local t t nil 1))))))
