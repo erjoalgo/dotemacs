53912957a925c5a909cc55a325108266f3fa9f38
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-24 12:11:37 -0400

    cider maybe auto-switch to relevant namespace

--- lisp/settings/cider-settings.el
+++ lisp/settings/cider-settings.el
@@ -1,4 +1,15 @@
 (add-hook 'cider-repl-mode-hook
 	  (lambda () (setenv "CIDER_REPL" "true")))
 
+(defun clojure-current-namespace ()
+  (when (eq major-mode 'clojure-mode)
+    (save-excursion
+      (goto-char (point-min))
+      (when (re-search-forward "^(ns \\(.*\\)")
+        (match-string 1)))))
 
+(defun cider-maybe-switch-to-current-ns ()
+  (when-let ((ns (clojure-current-namespace)))
+    (cider-interactive-eval (format "(ns %s)" ns))))
+
+(add-hook 'cider-connected-hook #'cider-maybe-switch-to-current-ns)
