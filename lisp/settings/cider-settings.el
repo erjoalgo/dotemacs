7d6e29431533825c18a185b077b2e43f78ce0c0b
Ernesto Alfonso <erjoalgo@gmail.com>
2023-10-28 15:07:12 -0400

    auto-switch to a known clojure namespace if possible

--- lisp/settings/cider-settings.el
+++ lisp/settings/cider-settings.el
@@ -8,8 +8,24 @@
       (when (re-search-forward "^(ns \\(.*\\)")
         (match-string 1)))))
 
+(defvar clojure-known-namespaces nil)
+
+(defun cider-add-to-know-namespaces ()
+    (when-let ((ns (clojure-current-namespace)))
+      (cl-pushnew ns clojure-known-namespaces  :test #'equal)))
+
+(add-hook 'clojure-mode-hook #'cider-add-to-know-namespaces)
+
 (defun cider-maybe-switch-to-current-ns ()
-  (when-let ((ns (clojure-current-namespace)))
-    (cider-interactive-eval (format "(ns %s)" ns))))
+  (when-let ((ns (and clojure-known-namespaces
+                      (selcand-select clojure-known-namespaces
+                                      :prompt "select among known namespaces: "
+                                      :autoselect-if-single t)))
+             (ns-form (format "(ns %s)" ns)))
+    (if (eq major-mode 'cider-repl-mode)
+        (progn (insert ns-form)
+             (cider-repl-return))
+        (cider-interactive-eval ns-form))))
 
 (add-hook 'cider-connected-hook #'cider-maybe-switch-to-current-ns)
+(add-hook 'cider-repl-mode #'cider-maybe-switch-to-current-ns)
