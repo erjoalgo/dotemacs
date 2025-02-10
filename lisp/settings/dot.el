db20069f4027980696f98e8f973973786100a9e9
Ernesto Alfonso <erjoalgo@gmail.com>
2025-02-10 15:37:47 -0500

    fix refernce to old function name

--- lisp/settings/dot.el
+++ lisp/settings/dot.el
@@ -2,6 +2,6 @@
   (when (or (cl-find major-mode '(dot-mode graphviz-mode))
             (equal (f-ext (buffer-file-name))
 		   "dot"))
-    (autobuild-run-action (autobuild-dot-to-ps))))
+    (autobuild--run-action (autobuild-dot-to-ps))))
 
 (add-hook 'after-save-hook 'maybe-export-dot-file)
