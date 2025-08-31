2c7c25babcd284943d4b91d6b6f30d79c640ddb4
Ernesto Alfonso <erjoalgo@gmail.com>
2025-08-31 12:51:05 -0400

    fix binding conflict ?

--- lisp/core/erjoalgo-command-mode.el
+++ lisp/core/erjoalgo-command-mode.el
@@ -533,7 +533,7 @@
        ("w" (file "~/.stumpwmrc.d/lisp/.stumpwmrc"))
        ("g" (file (file-truename "~/git")))
        ("3" (file (file-truename "~/git/3d")))
-       ("f" (file "~/git/dotemacs/lisp/extra/file-open.el"))
+       ("F" (file "~/git/dotemacs/lisp/extra/file-open.el"))
        ("n" (file "~/.stumpwmrc.d/bin"))
        ("N" (file "~/.stumpwmrc.d/installs"))
        ("W" (buff "*Warnings*"))
