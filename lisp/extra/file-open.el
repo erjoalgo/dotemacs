0a62acea8b0ba3f3975a5336fd5a351d53d5334d
Ernesto Alfonso <erjoalgo@gmail.com>
2025-11-30 20:19:49 -0500

    add avif as an image format

--- lisp/extra/file-open.el
+++ lisp/extra/file-open.el
@@ -25,7 +25,7 @@
 (def-open-file-program "libreoffice" ("doc" "ppt" "odt" "docx"  "pptx" "xls" "ods"))
 (def-open-file-program "evince" ("ps" "eps"))
 (def-open-file-program "qimgv"
-  ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg" "ico" "webp" "heic"))
+  ("png" "pgm" "tif" "jpg" "jpeg" "gif" "pnm" "svg" "ico" "webp" "heic" "avif"))
 (def-open-file-program "aplay" ("wav"))
 (def-open-file-program "timidity" ("midi" "mid"))
 (defun open-html-file (filename)
