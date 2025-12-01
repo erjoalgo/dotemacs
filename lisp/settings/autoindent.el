74a50880d56753e37753b090565cb79a08a90fdd
Ernesto Alfonso <erjoalgo@gmail.com>
2025-11-30 20:20:23 -0500

    merge indent-on-save files

--- lisp/settings/autoindent.el
+++ lisp/settings/autoindent.el
@@ -8,4 +8,10 @@
   (when (member major-mode autoindent-modes-list)
     (autoindent-indent-whole-buffer)))
 
-(add-hook 'before-save-hook #'autoindent-save-hook)
+
+;; (define-minor-mode indent-on-save-mode
+;;   "Mode to auto-indent on save"
+;;   :init-value 0
+;;   (funcall
+;;    (if indent-on-save-mode #'add-hook #'remove-hook)
+;;    #'before-save-hook #'autoindent-indent-whole-buffer))
